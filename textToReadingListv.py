adc872d3523e91b23d1b78211e276411b0138f1a
flips22 <1898114+flips22@users.noreply.github.com>
2024-01-13 07:59:07 -0600

    Removed truth DB check for text to xlsx as it is in the CBLgenerator

--- textToReadingListv.py
+++ textToReadingListv.py
@@ -2,23 +2,23 @@
 import re
 import os
 import pandas as pd
-import sqlite3
+#import sqlite3
 
 
 # Code doesn't handle something like: Captain America (2004) #1 – #9, #11 – #14
 
 rootDirectory = os.getcwd()
-dataDirectory = os.path.join(rootDirectory, "ReadingList-DB")
+#dataDirectory = os.path.join(rootDirectory, "ReadingList-DB")
 readingListDirectory = os.path.join(rootDirectory, "ReadingLists-Text")
 
 #truthDB = os.path.join(dataDirectory, "truthDB2.db")
 #truthDB = os.path.join(dataDirectory, "CMRO.db")
-truthDB = os.path.join(dataDirectory, "dbTruthTable-230312173308.db")
+#truthDB = os.path.join(dataDirectory, "dbTruthTable-230312173308.db")
 
-SearchDB = False
+#SearchDB = False
 
-inputFile = 'inputtest.txt'
-dbFile = 'cmrolistgood.db'
+#inputFile = 'inputtest.txt'
+#dbFile = 'cmrolistgood.db'
 
 yearPattern = r'\(([0-9]{4})\)'
 yearRemovalString = r' \([0-9]{4}\)'
@@ -57,14 +57,14 @@ def getRange(issuerange):
     
     return num_range
 
-useSeedYears = True
-seedStartYear = input("Enter seed start year, return for none: ")
-seedEndYear = input("Enter seed end year, return for none: ")
-print(re.match(seedYearPattern,seedStartYear))
-print(re.match(seedYearPattern,seedEndYear))
-if not re.match(seedYearPattern,seedStartYear) or not re.match(seedYearPattern,seedEndYear):
-    print('Invalid entry, not using seed years')
-    useSeedYears = False
+#useSeedYears = True
+#seedStartYear = input("Enter seed start year, return for none: ")
+#seedEndYear = input("Enter seed end year, return for none: ")
+#print(re.match(seedYearPattern,seedStartYear))
+#print(re.match(seedYearPattern,seedEndYear))
+#if not re.match(seedYearPattern,seedStartYear) or not re.match(seedYearPattern,seedEndYear):
+#    print('Invalid entry, not using seed years')
+#    useSeedYears = False
 
 for root, dirs, files in os.walk(readingListDirectory):
         for file in files:
@@ -75,8 +75,8 @@ for root, dirs, files in os.walk(readingListDirectory):
                 print('Processing %s'%(inputfile))
                 outputfile = file.replace('.txt', '.xlsx')
                 outputFile = os.path.join(readingListDirectory, outputfile)
-                touchjsonfile = outputfile.replace('.xlsx','') + '.json'
-                touchjsonFile = os.path.join(readingListDirectory, touchjsonfile)
+#                touchjsonfile = outputfile.replace('.xlsx','') + '.json'
+#                touchjsonFile = os.path.join(readingListDirectory, touchjsonfile)
                 if not os.path.exists(outputFile):
 
 
@@ -220,14 +220,22 @@ for root, dirs, files in os.walk(readingListDirectory):
                                             df.loc[index,'IssueNum'] = issue
                                             df.loc[index,'SeriesStartYear'] = year
                                             index += 1
+                    
+                    df.fillna({'SeriesStartYear':0}, inplace=True)
+                    df = df.astype({'IssueNum':str, 'SeriesStartYear':int})
 
-                    try:
-                        sqliteConnection = sqlite3.connect(truthDB,detect_types=sqlite3.PARSE_DECLTYPES)
-                        cursor = sqliteConnection.cursor()
+                    print(df)
+                    df.to_excel(outputFile)
+                    
 
-                    except sqlite3.Error as error:
-                        print("Error while connecting to sqlite", error)
 
+'''
+                    #try:
+                    #    sqliteConnection = sqlite3.connect(truthDB,detect_types=sqlite3.PARSE_DECLTYPES)
+                    #    cursor = sqliteConnection.cursor()
+
+                    #except sqlite3.Error as error:
+                    #    print("Error while connecting to sqlite", error)
                     for index, row in df.iterrows():
                         seriesName = row['SeriesName']
                         seriesStartYear = row['SeriesStartYear']
@@ -294,89 +302,8 @@ for root, dirs, files in os.walk(readingListDirectory):
 
                             # except:
                             #     print('No match found in DB file')
-
-                    df.fillna({'SeriesStartYear':0}, inplace=True)
-                    df = df.astype({'IssueNum':str, 'SeriesStartYear':int})
-
-                    print(df)
-                    df.to_excel(outputFile)
-                    
-                    open(touchjsonFile, 'w').close()
-
-                        
-                                    
-
-'''                                
-                                
-                                if ', ' in issuerange:
-                                    titlecommasplit = line.split(', ')
-                                    print(titlecommasplit[0])
-                                    print(titlecommasplit[1])
-                                    for issuerangeChunk in titlecommasplit:
-                                        if '–' in issuerangeChunk:
-                                            print(issuerangeChunk)
-                                            issuerangelist = getRange(issuerangeChunk)
-                                            for issue in issuerangelist:
-                                                #print(series)
-                                                #print(issue)
-                                                # if isinstance(issue, float):
-                                                #     issue = f'{issue:g}'
-                                                #issue = str(issue).replace('.0','')
-                                                #print(issue)
-                                                df.loc[index,'SeriesName'] = series
-                                                df.loc[index,'IssueNum'] = issue
-                                                index += 1
-                                        if '#' in issuerangeChunk and not '–' in issuerangeChunk:
-                                            issue = re.findall(r'\d+', issuerangeChunk)
-                                            df.loc[index,'SeriesName'] = series
-                                            df.loc[index,'IssueNum'] = issue[0]
-                                            index += 1
-                                    break
-                                if '–' in issuerange:
-                                    issuerangelist = getRange(issuerange)
-                                    for issue in issuerangelist:
-                                        print(series)
-                                        print(issue)
-                                        df.loc[index,'SeriesName'] = series
-                                        df.loc[index,'IssueNum'] = issue
-                                        index += 1
-                                else:    
-                                    #if '#' in issuerange and not '–' in issuerange:
-                                    issue = re.findall(r'\d+', issuerange)
-                                    df.loc[index,'SeriesName'] = series
-                                    df.loc[index,'IssueNum'] = issue[0]
-                                    index += 1
-                                # if '#' in line and not '–' in line:
-                                #     #print(line)
-                                #     titleNumSplit = line.split(' #')
-                                #     series = titleNumSplit[0]
-                                #     #issue = titleNumSplit[1]
-                                #     print(series)
-                                #     #print(issue)
-                                #     df.loc[index,'SeriesName'] = series
-                                #     issue = re.findall(r'\d+', titleNumSplit[1])
-                                #     df.loc[index,'IssueNum'] = issue[0]
-                                #     index += 1
-                                # if '–' in line:
-                                #     print('yep')
-                                #     # Define the input string
-                                #     titleNumSplit = line.split(' #')
-                                #     series = titleNumSplit[0]
-                                #     issuerangelist = getRange(titleNumSplit[1])
-                                    
-                                #     # # Use regular expression to find all numbers in the range
-                                #     # numbers = re.findall(r'\d+', line)
-
-                                #     # # Convert the numbers to integers and extract the range
-                                #     # num_range = list(range(int(numbers[0]), int(numbers[1])+1))
-
-                                #     # # Print the range of numbers
-                                #     print(issuerangelist)
-                                #     for issue in issuerangelist:
-                                #         print(series)
-                                #         print(issue)
-                                #         df.loc[index,'SeriesName'] = series
-                                #         df.loc[index,'IssueNum'] = issue
-                                #         index += 1
-
 '''
+
+                    #open(touchjsonFile, 'w').close()
+
+  

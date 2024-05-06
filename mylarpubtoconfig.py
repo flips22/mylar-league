9ad48442b7554148d00bf416b0cee9c6fd2cbe95
Cducharme84 <131784515+Cducharme84@users.noreply.github.com>
2024-05-06 16:16:46 -0400

    Removed text list of mylar pubs
    It was no longer used in the later versions of this script and could cause errors under weird conditions.

--- mylarpubtoconfig.py
+++ mylarpubtoconfig.py
@@ -40,12 +40,6 @@ def read_unique_values_from_database(database_path, column_name):
 
     return unique_values
 
-# Function to write unique values to a text file
-def write_to_text_file(values, file_path):
-    with open(file_path, 'w') as file:
-        for value in values:
-            file.write(value + '\n')
-
 # Function to match values from two text files and prepend numerical values
 def match_and_prepend_values(pubids_file_path, mylar_publishers_file_path):
     matched_values = []
@@ -97,10 +91,9 @@ def main():
     # Read unique values from database and write to text file
     unique_values = read_unique_values_from_database(database_path, 'ComicPublisher')
     mylar_publishers_file = 'mylarpublishers.txt'
-    write_to_text_file(unique_values, mylar_publishers_file)
 
     # Match values from two text files and prepend numerical values
-    pubids_file_path = 'superpubids.txt'
+    pubids_file_path = 'pubidsall.txt'
     match_and_prepend_values(pubids_file_path, mylar_publishers_file)
 
     # Parse numerical values and write back to config.ini
